b3f0d22b6738ab0bc90162f4c9bfa11cea1b05b2
Crisbravoon <cristobal_bravo6@hotmail.com>
2024-03-21 13:39:12 -0300

    Se agrego una carpeta, se hizo una tarea

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CallbackHook } from './08-Callback/CallbackHook'
+import { Padre } from './05-Tarea/Padre'
 // import { MemoHook } from './07-useMemo/MemoHook'
 // import { Memorize } from './06-useMemo/Memorize'
 // import { FocusScreen } from './04-useRef/FocusScreen'
+// import { CallbackHook } from './08-Callback/CallbackHook'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CallbackHook/>
+    <Padre/>
   </React.StrictMode>,
 )
